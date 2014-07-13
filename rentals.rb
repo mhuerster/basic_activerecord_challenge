08de3f22879dd2ce95519c8cf9ce0ed069f5b470
Vivek M George <vivek.m.george@gmail.com>
2014-07-13 14:51:32 -0400

    initial commit with tables and classes created as instructed

--- rentals.rb
+++ rentals.rb
@@ -5,38 +5,35 @@ require 'minitest/spec'
 
 ActiveRecord::Base.establish_connection adapter: "sqlite3", database: ":memory:"
 
-#
-# Define your migrations here, they should take the form of:
-#
-# ActiveRecord::Migration.create_table :fruits do |t|
-#   t.string :name
-#   t.belongs_to :bowl
-# end
-#
-# Repeat the above template for each table you need to create
+ActiveRecord::Migration.create_table :users do |t|
+  t.string :name
+end
+
+ActiveRecord::Migration.create_table :cars do |t|
+  t.string :type
+  t.string :color
+end
+
+ActiveRecord::Migration.create_table :rental_cars do |t|
+  t.references :user
+  t.references :car
+end
 
 
-#
-# end migrations
-#
-#
-# the following line executes the migrations, don't delete it
 ActiveRecord::Migrator.up "db/migrate"
 
+class User < ActiveRecord::Base
+  has_many :rental_cars
+  has_many :cars, through: :rental_cars 
+end
 
-#
-# Define your AR classes below:
-# for example:
-#
-# class Fruit < ActiveRecord::Base
-#   belongs_to :bowl
-# end
-#
-# You can define multiple classes, one after another
+class Car < ActiveRecord::Base
+  has_many :rental_cars
+  has_many :users, through: :users
+end
 
+class RentalCar < ActiveRecord::Base
+  belongs_to :car
+  belongs_to :user
+end
 
-
-
-
-#
-# end migrations
