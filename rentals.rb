55742c4d5d6508b1bbe2687fe2d1d926308ca539
Molly Huerster <mary.huerster@gmail.com>
2014-07-13 13:42:54 -0400

    user, car, rentals

--- rentals.rb
+++ rentals.rb
@@ -15,8 +15,27 @@ ActiveRecord::Base.establish_connection adapter: "sqlite3", database: ":memory:"
 #
 # Repeat the above template for each table you need to create
 
+ActiveRecord::Migration.create_table :users do |u|
+  u.string :name
+  u.integer :age
+  u.string :address
+  u.timestamps
+end
 
-#
+ActiveRecord::Migration.create_table :cars do |c|
+  c.string :make
+  c.string :model
+  c.integer :year
+  c.string :color
+  c.timestamps
+end
+
+ActiveRecord::Migration.create_table :rentals do |r|
+  r.references :user
+  r.references :car
+  r.timestamps
+end
+
 # end migrations
 #
 #
@@ -31,12 +50,20 @@ ActiveRecord::Migrator.up "db/migrate"
 # class Fruit < ActiveRecord::Base
 #   belongs_to :bowl
 # end
+
+class User < ActiveRecord::Base
+  has_many :rentals
+  has_many :cars, through: :rentals
+end
+
+class Car < ActiveRecord::Base
+  has_many :rentals
+  has_many :users, through: :rentals
+end
+
+class Rental < ActiveRecord::Base
+  has_many :users
+  has_many :cars
+end
 #
 # You can define multiple classes, one after another
-
-
-
-
-
-#
-# end migrations
