6562d960a217e709856b5a2296aa54a9363bdf49
Farheen Malik <fma2@github.com>
2014-07-13 13:38:00 -0400

    Added migrations and models

--- rentals.rb
+++ rentals.rb
@@ -14,8 +14,35 @@ ActiveRecord::Base.establish_connection adapter: "sqlite3", database: ":memory:"
 # end
 #
 # Repeat the above template for each table you need to create
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.timestamps
+    end
+  end
+end
 
+class CreateCars < ActiveRecord::Migration
+  def change
+    create_table :cars do |t|
+      t.string :rate
+      t.string :make
+      t.string :model
+      t.timestamps
+    end
+  end
+end
 
+class CreateRentalCars < ActiveRecord::Migration
+  def change
+    create_table :rental_cars do |t|
+      t.references :user
+      t.references :car
+      t.timestamps
+    end
+  end
+end
 #
 # end migrations
 #
@@ -34,6 +61,20 @@ ActiveRecord::Migrator.up "db/migrate"
 #
 # You can define multiple classes, one after another
 
+class User < ActiveRecord::Base
+  has_many :rental_cars
+  has_many :cars, :through => :rental_cars
+end
+
+
+class Car < ActiveRecord::Base
+  has_many :rental_cars
+  has_many :users, through: :rental_cars
+end
+
+class RentalCar < ActiveRecord::Base
+
+end
 
 
 
