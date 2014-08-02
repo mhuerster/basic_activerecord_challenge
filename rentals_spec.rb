2bbbac7ad282b200245c83f35668b32fded879a4
mhuerster <mary.huerster@gmail.com>
2014-08-02 13:49:20 -0400

    write tests for required methdods

--- rentals_spec.rb
+++ rentals_spec.rb
@@ -6,7 +6,7 @@ describe User do
 	# stretch - uncomment to run the test
 	xit "must be 25 or older" do
 		molly = User.create(name: "Molly", state: "NY", age: 23)
-		expect(molly.errors.messages[:age]).to include("must be greater than 25")
+		expect(molly.errors.messages[:age]).to include("must be greater than or equal to 25")
 	end
 	it "has many cars" do
 		sam = User.create(name: "Sam",
@@ -18,14 +18,14 @@ end
 
 describe Car do
 	it "has many rentals" do
-		car = Car.new(args)
+		car = Car.create(make:"Toyota", model:"Prius", state: "NY", license_plate: "AGY3559", year: 2010)
 		expect(car).to respond_to :rentals
 	end
 end
 
 describe RentalCar do
 	it "belongs to a user" do
-		rental_car = RentalCar.new(args)
+		rental_car = RentalCar.create()
 		expect(rental_car).to respond_to :customer
 	end
 end
